2cb309d02c231eb94e9d43ce6425523007946202
Roland <roland@riachi.com>
2019-07-17 11:21:33 -0400

    Update

---
+++ prereqs.js
@@ -0,0 +1,19 @@
+var cy = cytoscape({
+  container: document.getElementById('cy'),
+  zoomingEnabled: false,
+  elements: [
+    { data: { id: 'a' } },
+    { data: { id: 'b' } },
+    { data: { id: 'c' } },
+    { data: { id: 'd' } },
+    { data: { id: 'ab', source: 'a', target: 'b' } },
+    { data: { id: 'bc', source: 'b', target: 'c' } },
+    { data: { id: 'ac', source: 'a', target: 'c' } },
+    { data: { id: 'ad', source: 'a', target: 'd' } }
+  ]
+});
+
+document.getElementById('cy').width = '30%'
+// console.log(document.getElementById('cy').height);
+cy.resize();
+cy.fit();
