1fb08b86e0c8b55a0cb8de7e99a3d9f80e7d5db3
Roland <roland@riachi.com>
2019-08-13 14:10:16 -0400

    Added course information

---
+++ courseData/bio_tar.js
@@ -0,0 +1,822 @@
+global.bio_tar = {
+    "BIOL 101": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": [
+            "BIOL 111"
+        ]
+    },
+    "BIOL 102": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": [
+            "BIOL 112"
+        ]
+    },
+    "BIOL 111": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 112": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": [
+            "AEBI 122"
+        ]
+    },
+    "BIOL 115": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": [
+            "BIOL 111",
+            "BIOL 112"
+        ]
+    },
+    "BIOL 200": {
+        "prereqs": [
+            "BIOL 112"
+        ],
+        "coreqs": [
+            "CHEM 212"
+        ],
+        "restricts": [
+            "BIOL 219"
+        ]
+    },
+    "BIOL 201": {
+        "prereqs": [
+            "BIOL 200"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "BIOL 219",
+            "ANAT 212",
+            "BIOC 212"
+        ]
+    },
+    "BIOL 202": {
+        "prereqs": [
+            "BIOL 200",
+            "BIOL 219"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "CELL 204"
+        ]
+    },
+    "BIOL 205": {
+        "prereqs": [
+            "BIOL 200",
+            "BIOL 219",
+            "PHYS 101",
+            "PHYS 131"
+        ],
+        "coreqs": [
+            "BIOL 201",
+            "BIOL 219",
+            "ANAT 212",
+            "BIOC 212"
+        ],
+        "restricts": []
+    },
+    "BIOL 206": {
+        "prereqs": [
+            "BIOL 111"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 210": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 215": {
+        "prereqs": [
+            "BIOL 111"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "ENVR 202"
+        ]
+    },
+    "BIOL 219": {
+        "prereqs": [
+            "BIOL 112"
+        ],
+        "coreqs": [
+            "MATH 222"
+        ],
+        "restricts": [
+            "ANAT 212",
+            "BIOC 212",
+            "BIOL 200",
+            "BIOL 201"
+        ]
+    },
+    "BIOL 240": {
+        "prereqs": [
+            "BIOL 111"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "PLNT 358"
+        ]
+    },
+    "BIOL 300": {
+        "prereqs": [
+            "BIOL 200",
+            "BIOL 201",
+            "ANAT 212",
+            "BIOC 212",
+            "BIOL 219"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 301": {
+        "prereqs": [
+            "PHYS 102",
+            "PHYS 142",
+            "BIOL 200",
+            "BIOL 201",
+            "ANAT 212",
+            "BIOC 212",
+            "BIOL 219",
+            "BIOL 202",
+            "BIOL 206"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "BIOC 300"
+        ]
+    },
+    "BIOL 303": {
+        "prereqs": [
+            "BIOL 200",
+            "BIOL 201",
+            "ANAT 212",
+            "BIOC 212",
+            "BIOL 219",
+            "BIOL 300"
+        ],
+        "coreqs": [
+            "BIOL 202"
+        ],
+        "restricts": [
+            "ANAT 381"
+        ]
+    },
+    "BIOL 304": {
+        "prereqs": [
+            "BIOL 205",
+            "BIOL 215",
+            "ENVR 202"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 305": {
+        "prereqs": [
+            "BIOL 215",
+            "ENVR 200",
+            "ENVR 202"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 306": {
+        "prereqs": [
+            "PHYS 102",
+            "PHYS 142",
+            "BIOL 201",
+            "ANAT 212",
+            "BIOC 212",
+            "BIOL 219",
+            "NSCI 200"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 307": {
+        "prereqs": [
+            "BIOL 205",
+            "BIOL 215"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 308": {
+        "prereqs": [
+            "BIOL 215",
+            "ENVR 200",
+            "ENVR 202"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 309": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 310": {
+        "prereqs": [
+            "BIOL 215",
+            "ENVR 200",
+            "ENVR 202",
+            "MATH 112"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 313": {
+        "prereqs": [
+            "BIOL 200",
+            "BIOL 201",
+            "ANAT 212",
+            "BIOC 212",
+            "BIOL 219",
+            "BIOL 202"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 314": {
+        "prereqs": [
+            "BIOL 200",
+            "BIOL 201",
+            "ANAT 212",
+            "BIOC 212",
+            "BIOL 219"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 316": {
+        "prereqs": [
+            "BIOL 201",
+            "ANAT 212",
+            "BIOC 212",
+            "BIOL 219"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 319": {
+        "prereqs": [
+            "BIOL 200",
+            "BIOL 219",
+            "MATH 222",
+            "PHYS 230",
+            "PHYS 232",
+            "PHYS 253"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "PHYS 319"
+        ]
+    },
+    "BIOL 320": {
+        "prereqs": [
+            "NSCI 201",
+            "BIOL 306"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 324": {
+        "prereqs": [
+            "BIOL 202"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 331": {
+        "prereqs": [
+            "BIOL 206",
+            "BIOL 215"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 334": {
+        "prereqs": [
+            "BIOL 206",
+            "BIOL 215",
+            "ENVR 200",
+            "ENVR 202"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 335": {
+        "prereqs": [
+            "BIOL 205"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 342": {
+        "prereqs": [
+            "BIOL 205",
+            "BIOL 215",
+            "ENVR 200",
+            "ENVR 202"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 350": {
+        "prereqs": [
+            "BIOL 205"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "ENTO 330",
+            "ENTO 350"
+        ]
+    },
+    "BIOL 352": {
+        "prereqs": [
+            "BIOL 215",
+            "ENVR 202",
+            "EPSC 233"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 370": {
+        "prereqs": [
+            "BIOL 200",
+            "BIOL 201",
+            "ANAT 212",
+            "BIOC 212",
+            "BIOL 219",
+            "BIOL 202"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 373": {
+        "prereqs": [
+            "MATH 112"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 377": {
+        "prereqs": [
+            "BIOL 200",
+            "BIOL 201",
+            "ANAT 212",
+            "BIOC 212",
+            "BIOL 215",
+            "BIOL 219"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 385": {
+        "prereqs": [
+            "BIOL 205"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 389": {
+        "prereqs": [
+            "BIOL 306",
+            "NEUR 310",
+            "NSCI 200",
+            "PHGY 311"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 395": {
+        "prereqs": [
+            "BIOL 200",
+            "BIOL 219",
+            "CHEM 212",
+            "COMP 250",
+            "MATH 222"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 396": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 413": {
+        "prereqs": [
+            "BIOL 200",
+            "BIOL 201",
+            "ANAT 212",
+            "BIOC 212",
+            "BIOL 219",
+            "BIOL 202",
+            "BIOL 205",
+            "BIOL 215"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 416": {
+        "prereqs": [
+            "BIOL 202",
+            "BIOL 300",
+            "BIOL 303"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 418": {
+        "prereqs": [
+            "BIOL 205",
+            "BIOL 215",
+            "ENVR 200",
+            "ENVR 202"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 427": {
+        "prereqs": [
+            "BIOL 205",
+            "BIOL 305"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "BIOL 327"
+        ]
+    },
+    "BIOL 428": {
+        "prereqs": [
+            "BIOL 305"
+        ],
+        "coreqs": [
+            "BIOL 451",
+            "NRSC 451",
+            "ANTH 451",
+            "GEOG 451"
+        ],
+        "restricts": [
+            "BIOL 328"
+        ]
+    },
+    "BIOL 429": {
+        "prereqs": [
+            "BIOL 215"
+        ],
+        "coreqs": [
+            "BIOL 451",
+            "NRSC 451",
+            "ANTH 451",
+            "GEOG 451"
+        ],
+        "restricts": [
+            "BIOL 329"
+        ]
+    },
+    "BIOL 432": {
+        "prereqs": [
+            "BIOL 206",
+            "BIOL 215"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "ENVB 432"
+        ]
+    },
+    "BIOL 434": {
+        "prereqs": [
+            "BIOL 308",
+            "BIOL 309"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 436": {
+        "prereqs": [
+            "BIOL 304"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 441": {
+        "prereqs": [
+            "BIOL 215",
+            "ENVR 200",
+            "ENVR 202"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 451": {
+        "prereqs": [],
+        "coreqs": [
+            "ANTH 451",
+            "GEOG 451"
+        ],
+        "restricts": [
+            "NRSC 451"
+        ]
+    },
+    "BIOL 463": {
+        "prereqs": [
+            "BIOL 305",
+            "WILD 350"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 465": {
+        "prereqs": [
+            "BIOL 215",
+            "ENVR 200",
+            "ENVR 202"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 466": {
+        "prereqs": [
+            "BIOL 206",
+            "BIOL 301"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 468": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 469": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 479": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 499": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 507": {
+        "prereqs": [],
+        "coreqs": [
+            "BIOL 306",
+            "NEUR 310",
+            "NSCI 200",
+            "NSCI 201",
+            "PHGY 311",
+            "BIOL 307"
+        ],
+        "restricts": []
+    },
+    "BIOL 509": {
+        "prereqs": [
+            "BIOL 301",
+            "BIOL 304",
+            "BIOL 308"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 510": {
+        "prereqs": [
+            "BIOL 308",
+            "GEOG 350"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 514": {
+        "prereqs": [
+            "BIOL 306",
+            "NEUR 310",
+            "NSCI 201",
+            "PHGY 311"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "PSYC 514"
+        ]
+    },
+    "BIOL 515": {
+        "prereqs": [
+            "BIOL 432",
+            "BIOL 441"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 518": {
+        "prereqs": [
+            "BIOL 313"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 520": {
+        "prereqs": [
+            "BIOL 300",
+            "BIOL 303"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 524": {
+        "prereqs": [
+            "BIOL 300",
+            "BIOL 303"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 530": {
+        "prereqs": [
+            "BIOL 306",
+            "NSCI 200",
+            "PHGY 311"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 532": {
+        "prereqs": [
+            "BIOL 303",
+            "BIOL 306"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 540": {
+        "prereqs": [
+            "BIOL 308"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "ENVR 540"
+        ]
+    },
+    "BIOL 544": {
+        "prereqs": [
+            "BIOL 202",
+            "BIOL 300",
+            "BIOL 303"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 546": {
+        "prereqs": [
+            "BIOL 202",
+            "BIOL 300",
+            "BIOL 303"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 551": {
+        "prereqs": [
+            "CHEM 115",
+            "CHEM 120",
+            "MATH 133",
+            "MATH 141",
+            "PHYS 142",
+            "BIOL 201",
+            "BIOL 219",
+            "ANAT 212",
+            "BIOC 212",
+            "COMP 204",
+            "PHYS 230",
+            "BIOL 319",
+            "PHYS 319"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 553": {
+        "prereqs": [
+            "HISP 218",
+            "MATH 203",
+            "BIOL 215",
+            "ENVR 200",
+            "ENVR 202"
+        ],
+        "coreqs": [
+            "ENVR 451",
+            "HIST 510",
+            "GEOG 404"
+        ],
+        "restricts": []
+    },
+    "BIOL 568": {
+        "prereqs": [
+            "BIOL 202",
+            "BIOL 300",
+            "BIOL 370"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 569": {
+        "prereqs": [
+            "BIOL 303",
+            "BIOL 304"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 570": {
+        "prereqs": [],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 573": {
+        "prereqs": [
+            "BIOL 304",
+            "BIOL 352"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 575": {
+        "prereqs": [
+            "BIOL 202",
+            "BIOL 300"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 580": {
+        "prereqs": [
+            "BIOL 306",
+            "NSCI 200",
+            "NSCI 201",
+            "PHGY 311"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 588": {
+        "prereqs": [
+            "BIOL 300",
+            "BIOL 306"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 592": {
+        "prereqs": [
+            "BIOL 301"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "BINF 511"
+        ]
+    },
+    "BIOL 594": {
+        "prereqs": [
+            "BIOL 304",
+            "BIOL 308"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 596": {
+        "prereqs": [
+            "BIOL 373"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 597": {
+        "prereqs": [
+            "BIOL 373",
+            "BIOL 596"
+        ],
+        "coreqs": [],
+        "restricts": []
+    },
+    "BIOL 598": {
+        "prereqs": [
+            "BIOL 373"
+        ],
+        "coreqs": [],
+        "restricts": [
+            "BIOL 596",
+            "BIOL 597"
+        ]
+    }
+}
