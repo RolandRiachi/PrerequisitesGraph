dc6ffb6f6d8954dcb6fbfd627c32ccf6af36421f
Roland <roland@riachi.com>
2019-08-21 20:03:40 -0400

    More courses!

--- courseData/courses.js
+++ courseData/courses.js
@@ -1,20 +1,26 @@
 require('./eCalendar/math_tar.js')
 require('./eCalendar/math_src.js')
-require('./eCalendar/bio_tar.js')
-require('./eCalendar/bio_src.js')
+require('./eCalendar/biol_tar.js')
+require('./eCalendar/biol_src.js')
 require('./eCalendar/comp_tar.js')
 require('./eCalendar/comp_src.js')
 require('./eCalendar/phys_tar.js')
 require('./eCalendar/phys_src.js')
 require('./eCalendar/ecse_tar.js')
 require('./eCalendar/ecse_src.js')
+require('./eCalendar/econ_tar.js')
+require('./eCalendar/econ_src.js')
+require('./eCalendar/acct_tar.js')
+require('./eCalendar/acct_src.js')
 
 global.departments = {
     'MATH': [math_tar, math_src],
-    'BIOL': [bio_tar, bio_src],
+    'BIOL': [biol_tar, biol_src],
     'COMP': [comp_tar, comp_src],
     'PHYS': [phys_tar, phys_src],
-    'ECSE': [ecse_tar, ecse_src]
+    'ECSE': [ecse_tar, ecse_src],
+    'ECON': [econ_tar, econ_src],
+    'ACCT': [acct_tar, acct_src]
 }
 
 global.courses = [];
